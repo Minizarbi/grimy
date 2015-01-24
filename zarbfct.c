8db22e996630ca2bc682c8de5dd24630fe7e4d7c
Victor Adam <Victor.Adam@derpymail.org>
2015-01-24 17:37:51 +0100

    Colors of the rainbow!

--- zarbfct.c
+++ zarbfct.c
@@ -1,43 +1,53 @@
 #include <stdio.h>
 #include <math.h>
+#include <stropts.h>
+#include <termio.h>
 
 #define ROWS 80
 #define COLS 81
 
-#define RMAX 20.0
-#define RMIN 10.0
+#define RMAX 18.0
+#define RMIN 9.0
 
-#define VPTOUR 1.2
+#define VPTOUR 1.1
 #define PAS 0.05
 
-int fy(double i) {
+extern int usleep (__useconds_t __useconds);
+
+static int fx(double i) {
 	double rayvar = (RMIN + (RMAX - RMIN) / 2.0 + (RMAX + RMIN) / 2.0 * sin(i * VPTOUR));
-	return floor(ROWS / 2 + rayvar * sin(i));
+	return floor(ROWS / 2 + rayvar * sin(i)) - RMIN;
 }
 
-int fx(double i) {
+static int fy(double i) {
 	double rayvar = (RMIN + (RMAX - RMIN) / 2.0 + (RMAX + RMIN) / 2.0 * sin(i * VPTOUR));
 	return floor(COLS / 2 + rayvar * cos(i));
 }
 
-static char tab[ROWS][COLS] = {[0 ... ROWS - 1] = {[0 ... COLS - 2] = ' ', '\n'}};
-
 int main(void) {
-	int i, j;
 	double f;
+	struct termios oldT, newT;
+	char c;
+
+	ioctl(0,TCGETS,&oldT); /*get current mode*/
+
+	newT=oldT;
+
+	newT.c_lflag &= ~ECHO; /* echo off */
+	newT.c_lflag &= ~ICANON; /*one char @ a time*/
+	
+	ioctl(0,TCSETS,&newT); /* set new terminal mode */
 	
 	// Remplissage du tableau par des # selon deux fonctions fx et fy
 	for (f = 0.0; f < 1000.0; f += PAS) {
 		int x = fx(f);
 		int y = fy(f);
-		/* printf("%d, %d\n", x, y); */
-		tab[x][y] = '#';
-	}
-	
-	// Affichage du tableau
-	for (i = 0; i < ROWS; i++)
-		for (j = 0; j < COLS; j++)
-			printf("%c", tab[i][j]);
 
+		fflush(stdout);
+		printf("[%d;%dH%c", x, y, getchar());
+	}
+
+	ioctl(0,TCSETS,&oldT); /* restore previous terminal mode */
+	
 	return 0;
 }
